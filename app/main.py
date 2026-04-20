6c4b9dc23e5ffe8246d6b1b2612331b3c0d9b610
Sujoy Das <152258509+Alenbert009@users.noreply.github.com>
2026-04-20 19:16:00 +0530

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Logger
 logger = setup_logger()
 # Load model once
-model = load_model(MODEL_PATH)
+model = load_model(MODEL_PATH,compile=False)
 @app.get("/")
 def home():
     return {"message": "Deepfake Audio Detection API is running 🚀"}
