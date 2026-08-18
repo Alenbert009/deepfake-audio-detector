cdb5a6d299fe7b9c08a1a1b76132635cad63790f
Sujoy Das <152258509+Alenbert009@users.noreply.github.com>
2026-08-18 20:11:15 +0530

    Add files via upload

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -34,7 +34,7 @@ from app.app_logger import setup_logger
 # CONFIGURATION
 # =========================================================
 
-API_URL = "https://deepfake-audio-detector-6.onrender.com/"
+API_URL = "https://deepfake-audio-detector-6.onrender.com/predict"
 
 REQUEST_TIMEOUT = 120
 
